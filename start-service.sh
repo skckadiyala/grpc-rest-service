08d9257494f0f4800f1ade3c581465ee196c30ba
Kadiyala Suman Kalyan Chowdary <skchkadiyala@axway.com>
2019-07-19 11:34:34 -0700

    Added API Auth Basic and apikey for grpcs API Calls

--- start-service.sh
+++ start-service.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # export MONGODB_PASSWORD=$(kubectl get secret --namespace blogsvc mongodb -o jsonpath="{.data.mongodb-password}" | base64 --decode)
-#  blogserver -grpc-port=9090 -http-port=8080 -db-host=localhost:27017 -log-level=-1 -db-user=ampc -db-password=changeme
-# blogserver -grpc-port=9090 -http-port=8080 -db-host=mongodb.blogsvc:27017 -log-level=-1 -db-user=ampc -db-password=changeme
- blogserver -grpc-port=${GRPC_PORT} -http-port=${REST_PORT} -db-host=${MGODB_HOST} -log-level=-1 -db-user=${MGODB_USER} -db-password=${MGODB_PWD} -db-schema=${MGODB_SCHEMA} -db-collection=${MGODB_COLLECTION}
+#  blogserver -grpc-port=9090 -http-port=8080 -db-host=localhost:27017 -log-level=-1 -db-user=ampc -db-password=changeme -api-auth=true
+# blogserver -grpc-port=9090 -http-port=8080 -db-host=mongodb.blogsvc:27017 -log-level=-1 -db-user=ampc -db-password=changeme -api-auth=false
+ blogserver -grpc-port=${GRPC_PORT} -http-port=${REST_PORT} -db-host=${MGODB_HOST} -log-level=-1 -db-user=${MGODB_USER} -db-password=${MGODB_PWD} -db-schema=${MGODB_SCHEMA} -db-collection=${MGODB_COLLECTION} -api-auth=${API_AUTH}
